032a75e61d38e98907c412f47b86c8b4c54015ff
Маргарита <margaritatemlakova@gmail.com>
2026-07-18 12:09:39 +0300

    Выполнить первое практическое задание

--- krch/src/Main.java
+++ krch/src/Main.java
@@ -1,6 +1,28 @@
+import static java.lang.Character.isLetter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        }
+        String s = "J@va the be$t!123"; // надо перевернуть местами буквы, но не символы.
+        int n = s.length();  //17
+        char c = s.charAt(0);  //J
+        char[] chars = s.toCharArray();  //массив символов типа такого - ['J', 'a', 'v', 'a']
+
+        int left = 0;
+        int right = chars.length - 1;
+
+        while (left < right) {
+            if (!Character.isLetter(chars[left])) {  //если левый символ НЕ буква, идем дальше
+                left++;
+            } else if (!Character.isLetter(chars[right])) {  //если правый символ НЕ буква то идем дальше
+                right--;
+            } else {
+                char tmp = chars[left];
+                chars[left] = chars[right];  //переворачивает слово ЕСЛИ И ЛЕВО И ПРАВО ЯВЛЯЕТСЯ БУКВОЙ!!!
+                chars[right] = tmp;
+                left++;
+                right--;
+            }
+        }  // после всего вайл с массивом, переделываем массив с СТРОКУ, как в начале задания. пишем его.
+        System.out.println(new String(chars));  //массив символов стал строкой
     }
 }
